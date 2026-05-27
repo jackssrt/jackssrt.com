15ef0811c3a4c6ceaffb42e6b660de209b4f8a7a
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-05-28 01:22:25 +0300

    fix: fix centering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,8 +39,8 @@ i'm also interested in game development, modding, hardware and reverse engineeri
 	<Background />
 {/if}
 
-<div class="grid min-h-dvh place-items-center gap-2 px-2 py-2 pb-16">
-	<div class="flex flex-col gap-2 lg:w-xl">
+<div class="grid min-h-dvh place-items-center gap-2">
+	<div class="m-2 flex flex-col gap-2 lg:w-xl">
 		{@render children()}
 	</div>
 </div>
